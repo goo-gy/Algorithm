413047e1c269fcfa0681c4c4e7d4310ad5751e39
9oogy <lkukh17@gmail.com>
2021-11-29 15:54:19 +0900

    code refactoring

--- jo_data_structure/17_Peak/Peak.cpp
+++ jo_data_structure/17_Peak/Peak.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <list>
-#include <set>
 #include <queue>
-#include <algorithm>
+#include <set>
 #include <tuple>
+#include <algorithm>
 
 #define PAIR_II pair<int, int>
-
 #define TIME first
 #define TYPE second
 
-#define TYPE_BEGIN  1
-#define TYPE_END   -1
+enum { TYPE_END = -1, TYPE_BEGIN = 1 };
 
 using namespace std;
 
 set<int> s_timeBound;
 priority_queue<int, vector<int>, greater<int>> pq_timeBound;
 priority_queue<PAIR_II, vector<PAIR_II>, greater<PAIR_II>> pq_event; // time, type
-
+vector<int> v_timeBound;
+vector<int> v_count;
 int N;
 
 
 void solution()
 {
-    vector<int> v_count(pq_timeBound.size(), 0);
-    vector<int> v_timeBound;
-    list<PAIR_II> li_process;
-
+    v_count.resize(pq_timeBound.size(), 0);
     int i = 0;
     int countCur = 0;
     int timeCur = pq_timeBound.top();
+
     pq_timeBound.pop();
     v_timeBound.push_back(timeCur);
     while (!pq_event.empty())
@@ -41,7 +37,7 @@ void solution()
         {
             countCur += event.TYPE;
             v_count[i] = countCur;
-            pq_event.pop();
+            pq_event.pop(); 
         }
         else
         {
@@ -50,20 +46,6 @@ void solution()
             i++;
         }
     }
-
-    int maxBegin = v_timeBound[0];
-    int maxEnd = v_timeBound[1];
-    tuple<int, int, int> peakTime = make_tuple(v_count[0], maxEnd - maxBegin, maxEnd); // count, length, end
-    for (int i = 1; i < v_count.size() - 1; i++)
-    {
-        if (v_count[i] >= get<0>(peakTime))
-        {
-            maxBegin = v_timeBound[i];
-            maxEnd = v_timeBound[i + 1];
-            peakTime = max(peakTime, make_tuple(v_count[i], maxEnd - maxBegin, maxEnd));
-        }
-    }
-    cout << get<2>(peakTime) - get<1>(peakTime) << " " << get<2>(peakTime) << "\n";
 }
 
 void pre_setting()
@@ -97,6 +79,13 @@ void input()
 
 void output()
 {
+    tuple<int, int, int> peakTime = make_tuple(v_count[0], v_timeBound[1] - v_timeBound[0], v_timeBound[1]); // count, length, end
+    for (int i = 1; i < v_count.size() - 1; i++)
+    {
+        tuple<int, int, int> newRange = make_tuple(v_count[i], v_timeBound[i + 1] - v_timeBound[i], v_timeBound[i + 1]);
+        peakTime = max(peakTime, newRange);
+    }
+    cout << get<2>(peakTime) - get<1>(peakTime) << " " << get<2>(peakTime) << "\n";
 }
 
 int main()
